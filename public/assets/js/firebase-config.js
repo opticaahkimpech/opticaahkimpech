c18b2e5d3b92ae091af1a4237ffecb3e572485df
Brayan-chan <al071392@uacam.mx>
2025-05-18 14:58:52 -0600

    Agregar soporte para funciones de Firebase en la configuración

--- public/assets/js/firebase-config.js
+++ public/assets/js/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js"
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js"
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js"
+import { getFunctions } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-functions.js"
 
 // Configuración de Firebase
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const auth = getAuth(app)
 const db = getFirestore(app)
+const functions = getFunctions(app)
 
-export { app, auth, db }
+export { app, auth, db, functions }
